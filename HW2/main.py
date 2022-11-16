f1ecbcc2a88797364ca23cd7d8b4ac2ca1f58341
Matěj Frnka <mat.frnka@gmail.com>
2022-11-16 12:47:08 +0100

    hw2 - renaming

--- HW2/main.py
+++ HW2/main.py
@@ -35,12 +35,12 @@ class Apriori:
         return res
 
     @staticmethod
-    def check(candidate_subsets, s, k, data):
+    def check(candidate_subsets, support, k, data):
         """
         This function counts all occurrences of candidate_subsets in data and check if their
         count is above the given threshold s
         :param candidate_subsets: array of subsets to filter out the non-frequent items
-        :param s: support level
+        :param support: support
         :param k: size of sets in the subsets
         :param data: the dataset
         :return: filtered array of candidate subsets
@@ -52,30 +52,30 @@ class Apriori:
                 if frozenset(comb) in subset_cnt:
                     subset_cnt[frozenset(comb)] += 1
         # only return subsets that are frequent enough
-        threshold_cnt = len(data) * s
+        threshold_cnt = len(data) * support
         return [subset for subset, val in subset_cnt.items() if val > threshold_cnt]
 
     @staticmethod
-    def compute(s, k, data):
+    def find_frequent(support, k, data):
         """
         This function uses the A-priori pruning principle - if there is any item-set that is infrequent,
         its superset is also not frequent. The function starts with generating subsets of size 1 and then increases
         their size until they are of size k
-        :param s: support level, must be in range (0,1]
+        :param support: support, must be in range (0,1]
         :param k: find frequent sets of size k, must be an integer greater than 0
         :param data: array of sets representing the dataset
-        :return: array of frozen sets with frequency above the support level
+        :return: array of frozen sets with frequency above the support
         """
         assert k > 0
-        assert s >= 0
-        assert s <= 1
+        assert support >= 0
+        assert support <= 1
         t = time.time()
 
         # generate subsets of size 1
         l_1 = Apriori.get_combinations_l_1(data)
 
         # filter out the infrequent subsets
-        l_1 = Apriori.check(l_1, s, 1, data)
+        l_1 = Apriori.check(l_1, support, 1, data)
         print(time.time() - t)
         l_prev = l_1
         for i in range(2, k + 1):
@@ -85,12 +85,15 @@ class Apriori:
             l_curr = Apriori.get_combinations(l_1, l_prev)
 
             # filter out the infrequent subsets
-            l_curr = Apriori.check(l_curr, s, i, data)
+            l_curr = Apriori.check(l_curr, support, i, data)
 
             print(time.time() - t)
             l_prev = l_curr
         return l_prev
 
+    @staticmethod
+    def find_associated(support, confidence, data):
+        ...
 
 class DataLoader:
     res = []
@@ -111,4 +114,4 @@ class DataLoader:
 
 # d = {frozenset([1, 2, 3]), frozenset([1, 2]), frozenset([5, 6, 7, 8, 9]), frozenset([1, 2, 3, 4, 5, 6])}
 d = DataLoader.load("data/T10I4D100K.dat")
-print(Apriori().compute(0.01, 4, d))
+print(Apriori().find_frequent(0.01, 4, d))
